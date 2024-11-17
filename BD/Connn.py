b04904413fa9357d203b5ff8ea6ae5a41eecf5a2
Yoyo4052 <angelarroyo4051@gmail.com>
2024-11-16 22:51:43 -0700

    a

--- BD/Connn.py
+++ BD/Connn.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-databaseURL = "mysql://tesista:12345@localhost:3307/proto_db"
+databaseURL = "mysql://root:AxunnCqyVcNXuYrYmbernOvicOPIsOzu@autorack.proxy.rlwy.net:45891/railway"
 
 engine = create_engine(databaseURL)
 
